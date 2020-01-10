ca3fd7e4ce947b52473b52ba53f2f9c0c4e9fe93
HandiSutriyan <handisutrian@gmail.com>
2020-01-10 10:37:22 +0700

    add-post-override-def

--- polls/api/views.py
+++ polls/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
 
 from ..models import Question
 from ..serializers import QuestionSerializer
@@ -12,7 +14,21 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        SearchFilter,
+    )
     filter_fields = ('question_text',)
+    search_fields = ('question_text','id',)
+
+    #override function for method post
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # self.perform_create(serializer)
+        # headers = self.get_success_headers(serializer.data)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ChoiceViewSet(viewsets.ModelViewSet):
     queryset = Choice.objects.all()
--- polls/serializers.py
+++ polls/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from user.serializers import UserSerializer
 from .models import Question
@@ -6,10 +7,32 @@ from .models import Choice
 
 class QuestionSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(required = False)
+    created_by_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = Question
-        fields = ('__all__')
+        fields = (
+            'id',
+            'question_text',
+            'pub_date',
+
+            'created_by_id',
+            'created_by',
+        )
+    #override function for method post
+    def create(self, validated_data):
+        created_by_id = validated_data.get("created_by_id")
+        validated_data.pop("created_by_id", None)
+
+        q = Question(**validated_data)
+
+        #relate to user
+        user = User.objects.filter(id=created_by_id).first()
+        q.created_by = user
+        q.save()
+
+        return q
+       
 
 class QuestionDetailSerializer(serializers.ModelSerializer):
 
